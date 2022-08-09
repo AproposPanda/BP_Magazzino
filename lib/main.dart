11a1f480f56aaef11a9879a9cef9922a33c2865c
AproposPanda <110930989+AproposPanda@users.noreply.github.com>
2022-08-09 20:54:29 +0200

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,51 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-TextStyle text_style_standard =
+void main() => runApp(const MyApp());
+
+TextStyle Text_Style_Standard =
     GoogleFonts.lato(fontSize: 20, fontStyle: FontStyle.normal);
+TextStyle Text_Style_Title =
+    GoogleFonts.lato(fontSize: 24, fontWeight: FontWeight.bold);
 ButtonStyle style = ElevatedButton.styleFrom(
-    textStyle: text_style_standard,
+    textStyle: Text_Style_Standard,
     fixedSize: const Size(200, 100),
     primary: grigio);
 Color giallo = Color.fromARGB(247, 233, 174, 36);
 Color nero = Colors.black;
 Color grigio = Color.fromARGB(255, 230, 230, 230);
 
-void main() {
-  runApp(const MyApp());
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BP magazzino',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: "BP Magazzino",
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            width: 400,
+            height: 500,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20), color: grigio),
+            child: Column(
+              children: [
+                Text(
+                  "prova",
+                  style: Text_Style_Title,
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
 
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-          width: 400,
-          height: 500,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20), color: grigio)),
-    );
-  }
-}
+
+/* prova*/
